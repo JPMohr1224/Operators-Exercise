42c417b9518c313daf1372867c9ce7f9e57a4ca1
JP Mohr <James.Mohr2.0@hotmail.com>
2024-04-30 12:39:00 -0400

    Operators 1 & 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int minus = a - b;
+            int multiply = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+
+            AreaOfCircle();
+
+            // var is implicit type inferrence (more on this later)
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+
+            Console.WriteLine(k); // ouputs ? 16
         }
+
+        public static double AreaOfCircle()
+        {
+            Console.WriteLine("Please enter the raidus of the circle to caluclate the area.");
+            double radius = double.Parse(Console.ReadLine());
+            double solve = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {solve}.");
+            return solve;
+        }
+
+
+        
     }
 }
